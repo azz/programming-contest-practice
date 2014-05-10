dcea28fe2d3332cece845aca5cee422bfe437fcf
Lucas Azzola <lucas@lucasazzola.com>
2014-05-10 20:53:40 +1000

    fixed WA

--- uva/11530/main.c
+++ uva/11530/main.c
@@ -1,13 +1,14 @@
 // 11530 - SMS Typing
 #include <stdio.h>
 int main(void) {
-    size_t n, l, r, i, count, casenum = 1;
+    size_t n, l = 0, r, i, count, casenum = 1;
     char* buff = NULL;
-    int presses[27] = { 1, 2, 3,  1, 2, 3,   
+    int presses[26] = { 1, 2, 3,  1, 2, 3,   
               1, 2, 3,  1, 2, 3,  1, 2, 3,
-              1,2,3,4,  1, 2, 3,  1,2,3,4, };
-    for (scanf("%zu\n", &n); n > 0 && (r = getline(&buff, &l, stdin)) != -1; n--) {
-        for (i = 0, count = 0; i+1 < r; i++) 
+              1,2,3,4,  1, 2, 3,  1,2,3,4  };
+    scanf("%zu", &n); getchar(); 
+    while (n-- > 0 && (r = getline(&buff, &l, stdin)) != -1) {
+        for (i = 0, count = 0; buff[i] != '\n' && i < r; i++)
             count += (buff[i] == ' ') ? 1 : presses[buff[i] - 'a'];
         printf("Case #%zu: %zu\n", casenum++, count);
     }
